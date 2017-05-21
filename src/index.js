12606ede4dc2ff2b03901e9cb2692f3de64e40bc
scotchfield <swrittenb@gmail.com>
2017-05-21 15:46:59 -0400

    Adds Preact selector component and map for app rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import { h, render } from 'preact';
+import { h, render, Component } from 'preact';
+
+class Selector extends Component {
+  render(props) {
+    return (
+      <div className="selector">
+        {props.number}
+      </div>
+    );
+  }
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 render((
-    <div id="foo">
-        <span>Hello, world!</span>
-        <button onClick={ e => alert("hi!") }>Click Me</button>
-    </div>
-), document.body);
+  <div id="app">
+    { numbers.map(x => <Selector number={x} />) }
+  </div>
+), document.getElementById('app'));
